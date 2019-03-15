43909e894bb6be338bf91d296f7d825bc2f05002
Mihai <mihai.ocneanu@gmail.com>
2019-03-15 20:46:31 +0200

    added people

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -151,6 +151,7 @@ module.exports = {
                     'menu-manager',
                     'multitenancy',
                     'notifications',
+                    'people',
                 ]
             }],
         }
