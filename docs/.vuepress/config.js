243668382c77d856e33fb168c16509d0c23ea276
Mihai <mihai.ocneanu@gmail.com>
2019-03-15 15:27:06 +0200

    added file manager

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -140,6 +140,7 @@ module.exports = {
                     'data-import',
                     'discussions',
                     'documents-manager',
+                    'file-manager',
                 ]
             }],
         }
