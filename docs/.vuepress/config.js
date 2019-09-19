a73e86bc5452377211d6e9f9b5d069478b4e7517
Mihai <mihai.ocneanu@gmail.com>
2019-09-19 14:35:29 +0300

    added products documentation

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -122,6 +122,7 @@ module.exports = {
                     'pdf',
                     'people',
                     'permissions',
+                    'products',
                     'rememberable',
                     'roles',
                     'searchable',
